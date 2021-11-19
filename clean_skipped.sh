9fe5ae1f26dd3416cb2df7fb08f520193cddfbb4
Isabel A Gerrard <iag@Isabels-MacBook-Pro.local>
2021-11-19 13:35:08 -0800

    updated with comments

--- clean_skipped.sh
+++ clean_skipped.sh
@@ -1,4 +1,4 @@
-# arg 1 is source dir, 2 is target, arg 3 is skipped file
+# arg 1 is source dir, 2 is target, arg 3 is skipped file, 4 is fail output file
 #mkdir $1
 #mkdir $2
 #cd $1
@@ -8,9 +8,11 @@ do
   echo "Downloading ${url}"
   if wget -q $url -P $1; then
     echo "Downloaded"
-    blender --background --python ./glb.py -- $1 $2 $url
+    blender --background --python ./glb.py -- ./$1 ./$2 $line
   else
     echo "Failed"
-    echo "${url}" >> ./$3
+    echo "${line}" >> ./$4
   fi
+
+  # upload $4 to firebase
 done
